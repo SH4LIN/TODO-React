8dcd850a13e89ffafccbe41cc8b9e262e963f686
Shalin Shah <sahilshh723@gmail.com>
2023-02-17 15:37:18 +0530

    Assignment 3 for rt-person custom metaboxes like birth date birth place and social information like  twitter url facebook url website url and instagram url is added

--- plugins/movie-library/admin/classes/class-movie-library-meta-boxes.php
+++ plugins/movie-library/admin/classes/class-movie-library-meta-boxes.php
@@ -7,6 +7,7 @@
 
 namespace MovieLib\admin\classes;
 
+use WP_Post;
 use WP_Query;
 
 /**
@@ -26,47 +27,164 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Meta_Boxes' ) ) {
 		 * This function is used to add the meta boxes for the plugin.
 		 */
 		public function add_meta_boxes(): void {
-			$screens = array( 'rt-movie' );
-			foreach ( $screens as $screen ) {
-				$meta_boxes_args = $this->get_meta_boxes_args( $screen );
-				foreach ( $meta_boxes_args as $meta_box_id => $meta_box_args ) {
-					add_meta_box(
-						$meta_box_id,
-						$meta_box_args[ 'title' ],
-						$meta_box_args[ 'callback' ],
-						$meta_box_args[ 'screen' ],
-						$meta_box_args[ 'context' ],
-						$meta_box_args[ 'priority' ]
-					);
-				}
+
+			$meta_boxes_args = $this->get_meta_boxes_args();
+			foreach ( $meta_boxes_args as $meta_box_id => $meta_box_args ) {
+				add_meta_box(
+					$meta_box_id,
+					$meta_box_args[ 'title' ],
+					$meta_box_args[ 'callback' ],
+					$meta_box_args[ 'screen' ],
+					$meta_box_args[ 'context' ],
+					$meta_box_args[ 'priority' ]
+				);
 			}
 		}
 
-		private function get_meta_boxes_args( $screen ): array {
+		private function get_meta_boxes_args(): array {
 			return array(
-				'rt-movie-meta-basic' => array(
+				'rt-movie-meta-basic'   => array(
 					'title'    => __( 'Basic', 'movie-library' ),
 					'callback' => array( $this, 'rt_movie_meta_basic' ),
-					'screen'   => $screen,
+					'screen'   => [ 'rt-movie' ],
 					'context'  => 'side',
 					'priority' => 'high',
 				),
-				'rt-movie-meta-crew'  => array(
+				'rt-movie-meta-crew'    => array(
 					'title'    => __( 'Crew', 'movie-library' ),
 					'callback' => array( $this, 'rt_movie_meta_crew' ),
-					'screen'   => $screen,
+					'screen'   => [ 'rt-movie' ],
+					'context'  => 'side',
+					'priority' => 'high',
+				),
+				'rt-person-meta-basic'  => array(
+					'title'    => __( 'Basic', 'movie-library' ),
+					'callback' => array( $this, 'rt_person_meta_basic' ),
+					'screen'   => [ 'rt-person' ],
+					'context'  => 'side',
+					'priority' => 'high',
+				),
+				'rt-person-meta-social' => array(
+					'title'    => __( 'Basic', 'movie-library' ),
+					'callback' => array( $this, 'rt_person_meta_social' ),
+					'screen'   => [ 'rt-person' ],
 					'context'  => 'side',
 					'priority' => 'high',
 				),
 			);
 		}
 
+		public function rt_person_meta_social( WP_Post $post ): void {
+			$rt_person_meta_social_data = get_post_meta( $post->ID );
+
+			$rt_person_meta_social_key = array(
+				'twitter'   => 'rt-person-meta-social-twitter',
+				'facebook'  => 'rt-person-meta-social-facebook',
+				'instagram' => 'rt-person-meta-social-instagram',
+				'website'   => 'rt-person-meta-social-web',
+			);
+			wp_nonce_field( 'rt_person_meta_nonce', 'rt_person_meta_nonce' );
+			?>
+
+			<div class="rt-person-meta-fields rt-person-meta-social-fields">
+				<div class="rt-person-meta-container rt-person-meta-social-container rt-person-meta-social-twitter-container">
+					<label class="rt-person-meta-label rt-person-meta-social-label rt-person-meta-social-twitter-label" for="<?php echo esc_attr( $rt_person_meta_social_key[ 'twitter' ] ); ?>">
+						<?php esc_html_e( 'Twitter', 'movie-library' ); ?>
+					</label>
+					<input type="text"
+							value="<?php echo esc_url( $rt_person_meta_social_data[ $rt_person_meta_social_key[ 'twitter' ] ][ 0 ] ) ?>"
+							class="rt-person-meta-field rt-person-meta-social-field rt-person-meta-social-twitter-field"
+							name="<?php echo esc_attr( $rt_person_meta_social_key[ 'twitter' ] ); ?>"
+							id="<?php echo esc_attr( $rt_person_meta_social_key[ 'twitter' ] ); ?>"
+					/>
+				</div>
+				<div class="rt-person-meta-container rt-person-meta-social-container rt-person-meta-social-facebook-container">
+					<label class="rt-person-meta-label rt-person-meta-social-label rt-person-meta-social-facebook-label" for="<?php echo esc_attr( $rt_person_meta_social_key[ 'facebook' ] ); ?>">
+						<?php esc_html_e( 'Facebook', 'movie-library' ); ?>
+					</label>
+					<input type="text"
+							value="<?php echo esc_url( $rt_person_meta_social_data[ $rt_person_meta_social_key[ 'facebook' ] ][ 0 ] ) ?>"
+							class="rt-person-meta-field rt-person-meta-social-field rt-person-meta-social-facebook-field"
+							name="<?php echo esc_attr( $rt_person_meta_social_key[ 'facebook' ] ); ?>"
+							id="<?php echo esc_attr( $rt_person_meta_social_key[ 'facebook' ] ); ?>"
+					/>
+				</div>
+				<div class="rt-person-meta-container rt-person-meta-social-container rt-person-meta-social-instagram-container">
+					<label class="rt-person-meta-label rt-person-meta-social-label rt-person-meta-social-instagram-label" for="<?php echo esc_attr( $rt_person_meta_social_key[ 'instagram' ] ); ?>">
+						<?php esc_html_e( 'Instagram', 'movie-library' ); ?>
+					</label>
+					<input type="text"
+							value="<?php echo esc_url( $rt_person_meta_social_data[ $rt_person_meta_social_key[ 'instagram' ] ][ 0 ] ) ?>"
+							class="rt-person-meta-field rt-person-meta-social-field rt-person-meta-social-instagram-field"
+							name="<?php echo esc_attr( $rt_person_meta_social_key[ 'instagram' ] ); ?>"
+							id="<?php echo esc_attr( $rt_person_meta_social_key[ 'instagram' ] ); ?>"
+					/>
+				</div>
+				<div class="rt-person-meta-container rt-person-meta-social-container rt-person-meta-social-website-container">
+					<label class="rt-person-meta-label rt-person-meta-social-label rt-person-meta-social-website-label" for="<?php echo esc_attr( $rt_person_meta_social_key[ 'Website' ] ); ?>">
+						<?php esc_html_e( 'Website', 'movie-library' ); ?>
+					</label>
+					<input type="text"
+							value="<?php echo esc_url( $rt_person_meta_social_data[ $rt_person_meta_social_key[ 'website' ] ][ 0 ] ) ?>"
+							class="rt-person-meta-field rt-person-meta-social-field rt-person-meta-social-website-field"
+							name="<?php echo esc_attr( $rt_person_meta_social_key[ 'website' ] ); ?>"
+							id="<?php echo esc_attr( $rt_person_meta_social_key[ 'website' ] ); ?>"/>
+				</div>
+			</div>
+			<?php
+		}
+
 		/**
+		 * @function rt_movie_meta_crew
+		 * This function is used to display the meta box for the movie crew.
+		 *
+		 * @param WP_Post $post The post object.
+		 */
+		public function rt_person_meta_basic( WP_Post $post ): void {
+			$rt_person_meta_basic_data = get_post_meta( $post->ID );
+
+			$rt_person_meta_basic_key = array(
+				'birth-date'  => 'rt-person-meta-basic-birth-date',
+				'birth-place' => 'rt-person-meta-basic-birth-place',
+			);
+			wp_nonce_field( 'rt_person_meta_nonce', 'rt_person_meta_nonce' );
+			?>
+
+			<div class="rt-person-meta-fields rt-person-meta-basic-fields">
+				<div class="rt-person-meta-container rt-person-meta-basic-container rt-person-meta-basic-birth-date-container">
+					<label class="rt-person-meta-label rt-person-meta-basic-label rt-person-meta-basic-birth-date-label" for="<?php echo esc_attr( $rt_person_meta_basic_key[ 'birth-date' ] ); ?>">
+						<?php esc_html_e( 'Birth Date', 'movie-library' ); ?>
+					</label>
+					<input type="date"
+							value="<?php echo esc_attr( $rt_person_meta_basic_data[ $rt_person_meta_basic_key[ 'birth-date' ] ][ 0 ] ) ?>"
+							class="rt-person-meta-field rt-person-meta-basic-field rt-person-meta-basic-birth-date-field"
+							name="<?php echo esc_attr( $rt_person_meta_basic_key[ 'birth-date' ] ); ?>"
+							id="<?php echo esc_attr( $rt_person_meta_basic_key[ 'birth-date' ] ); ?>"/>
+				</div>
+				<div class="rt-person-meta-container rt-person-meta-basic-container rt-person-meta-basic-birth-place-container">
+					<label class="rt-person-meta-label rt-person-meta-basic-label rt-person-meta-basic-birth-place-label" for="<?php echo esc_attr( $rt_person_meta_basic_key[ 'birth-place' ] ); ?>">
+						<?php esc_html_e( 'Birth Place', 'movie-library' ); ?>
+					</label>
+					<input type="text"
+							value="<?php echo esc_attr( $rt_person_meta_basic_data[ $rt_person_meta_basic_key[ 'birth-place' ] ][ 0 ] ) ?>"
+							class="rt-person-meta-field rt-person-meta-basic-field rt-person-meta-basic-birth-place-field"
+							name="<?php echo esc_attr( $rt_person_meta_basic_key[ 'birth-place' ] ); ?>"
+							id="<?php echo esc_attr( $rt_person_meta_basic_key[ 'birth-place' ] ); ?>"/>
+				</div>
+
+
+			</div>
+
+			<?php
+		}
+
+		/**
+		 * @function rt_movie_meta_basic
 		 * This function is used to display the meta box for the movie details.
 		 *
-		 * @param object $post The post object.
+		 * @param WP_Post $post The post object.
 		 */
-		public function rt_movie_meta_basic( $post ): void {
+		public function rt_movie_meta_basic( WP_Post $post ): void {
 			$rt_movie_meta_basic_data = get_post_meta( $post->ID );
 
 			$rt_movie_meta_basic_key = array(
@@ -84,7 +202,7 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Meta_Boxes' ) ) {
 					</label>
 					<input type="number"
 							value="<?php echo esc_attr( $rt_movie_meta_basic_data[ $rt_movie_meta_basic_key[ 'rating' ] ][ 0 ] ) ?>"
-							class="rt-movie-meta-basic-field rt-movie-meta-basic-rating-field"
+							class="rt-movie-meta-field rt-movie-meta-basic-field rt-movie-meta-basic-rating-field"
 							name="<?php echo esc_attr( $rt_movie_meta_basic_key[ 'rating' ] ); ?>"
 							id="<?php echo esc_attr( $rt_movie_meta_basic_key[ 'rating' ] ); ?>"
 							max="5"
@@ -96,7 +214,7 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Meta_Boxes' ) ) {
 					</label>
 					<input type="number"
 							value="<?php echo esc_attr( $rt_movie_meta_basic_data[ $rt_movie_meta_basic_key[ 'runtime' ] ][ 0 ] ) ?>"
-							class="rt-movie-meta-basic-field rt-movie-meta-basic-runtime-field"
+							class="rt-movie-meta-field rt-movie-meta-basic-field rt-movie-meta-basic-runtime-field"
 							name="<?php echo esc_attr( $rt_movie_meta_basic_key[ 'runtime' ] ); ?>"
 							id="<?php echo esc_attr( $rt_movie_meta_basic_key[ 'runtime' ] ); ?>"
 							min="1"
@@ -109,7 +227,7 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Meta_Boxes' ) ) {
 					</label>
 					<input type="date"
 							value="<?php echo esc_attr( $rt_movie_meta_basic_data[ $rt_movie_meta_basic_key[ 'release-date' ] ][ 0 ] ) ?>"
-							class="rt-movie-meta-basic-field rt-movie-meta-basic-release-date-field"
+							class="rt-movie-meta-field rt-movie-meta-basic-field rt-movie-meta-basic-release-date-field"
 							name="<?php echo esc_attr( $rt_movie_meta_basic_key[ 'release-date' ] ); ?>"
 							id="<?php echo esc_attr( $rt_movie_meta_basic_key[ 'release-date' ] ); ?>"/>
 				</div>
@@ -119,6 +237,87 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Meta_Boxes' ) ) {
 			<?php
 		}
 
+		/**
+		 * @function rt_movie_meta_crew
+		 * This function is used to display the meta box for the movie crew.
+		 *
+		 * @param WP_Post $post The post object.
+		 */
+		public function rt_movie_meta_crew( WP_Post $post ): void {
+			$rt_career_terms = get_terms(
+				[
+					'taxonomy'   => 'rt-person-career',
+					'hide_empty' => false,
+				]
+			);
+
+			$rt_people_data          = array();
+			$rt_movie_meta_crew_data = array();
+			foreach ( $rt_career_terms as $rt_career_term ) {
+				$key                                     = 'rt-movie-meta-crew-' . $rt_career_term->slug;
+				$crew_data                               = get_post_meta( $post->ID, $key );
+				$rt_movie_meta_crew_data[ $key ]         = $crew_data;
+				$rt_person_data                          = $this->get_person_data( $rt_career_term );
+				$rt_people_data[ $rt_career_term->name ] = $rt_person_data;
+			}
+			wp_nonce_field( 'rt_movie_meta_nonce', 'rt_movie_meta_nonce' );
+
+			?>
+
+			<div class="rt-movie-meta-fields rt-movie-meta-crew-fields">
+				<?php
+				foreach ( $rt_people_data as $key => $data ) {
+					if ( empty( $data ) ) {
+						continue;
+					}
+					?>
+					<div class="rt-movie-meta-container rt-movie-meta-crew-container <?php echo esc_attr( strtolower( 'rt-movie-meta-crew-' . $key . '-container' ) ) ?>">
+						<label class="rt-movie-meta-label rt-movie-meta-crew-label <?php echo esc_attr( strtolower( 'rt-movie-meta-crew-' . $key . '-label' ) ) ?>" for="<?php echo esc_attr( strtolower( str_replace( '-', '_', 'rt-movie-meta-crew-' . $key ) ) ) ?>">
+							<?php esc_html_e( $key, 'movie-library' ); ?>
+						</label>
+						<select class="rt-movie-meta-field rt-movie-meta-crew-field <?php echo esc_attr( strtolower( 'rt-movie-meta-crew-' . $key . '-field' ) ) ?>"
+								name="<?php echo esc_attr( strtolower( 'rt-movie-meta-crew-' . $key ) . '[]' ); ?>"
+								id="<?php echo esc_attr( strtolower( str_replace( '-', '_', 'rt-movie-meta-crew-' . $key ) ) ) ?>" multiple="multiple">
+
+							<option value="" disabled> <?php echo esc_html__( "Select" ) . esc_html( $key ); ?></option>
+							<?php
+
+							foreach ( $data as $rt_p ) {
+								if ( empty( $rt_movie_meta_crew_data[ strtolower( 'rt-movie-meta-crew-' . $key ) ] ) ) {
+									printf(
+										'<option value="%1$d">%2$s</option>',
+										esc_attr( $rt_p[ 'id' ] ),
+										esc_html( $rt_p[ 'name' ] )
+									);
+								} else {
+									$selected = in_array( $rt_p[ 'id' ], $rt_movie_meta_crew_data[ strtolower( 'rt-movie-meta-crew-' . $key ) ][ 0 ] ) ? 'selected' : '';
+									printf(
+										'<option value="%1$d" "%2$s">%3$s</option>',
+										esc_attr( $rt_p[ 'id' ] ),
+										$selected,
+										esc_html( $rt_p[ 'name' ] ),
+
+									);
+								}
+							} ?>
+						</select>
+					</div>
+					<?php
+				}
+				?>
+			</div>
+
+			<?php
+		}
+
+		/**
+		 * @function get_person_data
+		 * This function is used to get the person data.
+		 *
+		 * @param object $rt_career_term The career term object.
+		 *
+		 * @return array
+		 */
 		private function get_person_data( $rt_career_term ): array {
 			$rt_person_data  = array();
 			$rt_person_query = new WP_Query(
@@ -148,73 +347,6 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Meta_Boxes' ) ) {
 
 			return $rt_person_data;
 		}
-
-		public function rt_movie_meta_crew( $post ): void {
-			$rt_career_terms = get_terms(
-				[
-					'taxonomy'   => 'rt-person-career',
-					'hide_empty' => false,
-				]
-			);
-
-			$rt_people_data          = array();
-			$rt_movie_meta_crew_data = array();
-			foreach ( $rt_career_terms as $rt_career_term ) {
-				$key                                     = 'rt-movie-meta-crew-' . $rt_career_term->slug;
-				$crew_data                               = get_post_meta( $post->ID, $key );
-				$rt_movie_meta_crew_data[ $key ]         = $crew_data;
-				$rt_person_data                          = $this->get_person_data( $rt_career_term );
-				$rt_people_data[ $rt_career_term->name ] = $rt_person_data;
-			}
-			wp_nonce_field( 'rt_movie_meta_nonce', 'rt_movie_meta_nonce' );
-
-			?>
-
-
-			<div class="rt-movie-meta-fields rt-movie-meta-crew-fields">
-				<?php
-				foreach ( $rt_people_data as $key => $data ) {
-					if ( empty( $data ) ) {
-						continue;
-					}
-					?>
-					<div class="rt-movie-meta-container rt-movie-meta-crew-container <?php echo esc_attr( strtolower( 'rt-movie-meta-crew-' . $key . '-container' ) ) ?>">
-						<label class="rt-movie-meta-label rt-movie-meta-crew-label <?php echo esc_attr( strtolower( 'rt-movie-meta-crew-' . $key . '-label' ) ) ?>" for="<?php echo esc_attr( strtolower( str_replace( '-', '_', 'rt-movie-meta-crew-' . $key ) ) ) ?>">
-							<?php esc_html_e( $key, 'movie-library' ); ?>
-						</label>
-						<select class="rt-movie-meta-crew-field <?php echo esc_attr( strtolower( 'rt-movie-meta-crew-' . $key . '-field' ) ) ?>"
-								name="<?php echo esc_attr( strtolower( 'rt-movie-meta-crew-' . $key ) . '[]' ); ?>"
-								id="<?php echo esc_attr( strtolower( str_replace( '-', '_', 'rt-movie-meta-crew-' . $key ) ) ) ?>" multiple="multiple">
-
-							<option value="" disabled> <?php echo esc_html__( "Select" ) . esc_html( $key ); ?></option>
-							<?php
-
-							foreach ( $data as $rt_p ) {
-								if ( empty( $rt_movie_meta_crew_data[ strtolower( 'rt-movie-meta-crew-' . $key ) ] ) ) {
-									printf(
-										'<option value="%1$d">%2$s</option>',
-										esc_attr( $rt_p[ 'id' ] ),
-										esc_html( $rt_p[ 'name' ] )
-									);
-								} else {
-									$selected = in_array( $rt_p[ 'id' ], $rt_movie_meta_crew_data[ strtolower( 'rt-movie-meta-crew-' . $key ) ][ 0 ] ) ? 'selected' : '';
-									printf(
-										'<option value="%1$d" %2$s>%3$s</option>',
-										esc_attr( $rt_p[ 'id' ] ),
-										$selected,
-										esc_html( $rt_p[ 'name' ] ),
-
-									);
-								}
-							} ?>
-						</select>
-					</div>
-					<?php
-				}
-				?>
-			</div>
-
-			<?php
-		}
 	}
 }
+
--- plugins/movie-library/admin/classes/class-movie-library-save-post.php
+++ plugins/movie-library/admin/classes/class-movie-library-save-post.php
@@ -7,6 +7,8 @@
 
 namespace MovieLib\admin\classes;
 
+use WP_Post;
+
 /**
  * This is a security measure to prevent direct access to the file.
  */
@@ -19,26 +21,102 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Save_Post' ) ) {
 	 * This class is used to handle all the operation while saving the post.
 	 */
 	class Movie_Library_Save_Post {
-		public function save_post( $post_id, $post, $update ): void {
+		public function save_post( int $post_id, WP_Post $post, bool $update ): void {
 			if ( wp_is_post_autosave( $post_id ) || wp_is_post_revision( $post_id ) ) {
 				return;
 			}
-			$post_type = get_post_type( $post_id );
 			// Check the user's permissions.
-			if ( isset( $_POST[ 'post_type' ] ) && ( $_POST[ 'post_type' ] === 'rt-movie' || $post_type === 'rt-movie' ) ) {
-				if ( ! current_user_can( 'edit_page', $post_id ) ) {
-					return;
+			if ( isset( $_POST[ 'post_type' ] ) ) {
+				if ( $_POST[ 'post_type' ] === 'rt-movie' ) {
+					if ( ! current_user_can( 'edit_page', $post_id ) ) {
+						return;
+					} else {
+						$this->save_rt_movie_post( $post_id, $post, $update );
+					}
+				} elseif ( $_POST[ 'post_type' ] === 'rt-person' ) {
+					if ( ! current_user_can( 'edit_page', $post_id ) ) {
+						return;
+					} else {
+						$this->save_rt_person_post( $post_id, $post, $update );
+					}
 				} else {
-					$this->save_rt_movie_post( $post_id, $post, $update );
-				}
-			} else {
-				if ( ! current_user_can( 'edit_post', $post_id ) ) {
-					return;
+					if ( ! current_user_can( 'edit_post', $post_id ) ) {
+						return;
+					}
 				}
 			}
 		}
 
-		private function save_rt_movie_post( $post_id, $post, $update ): void {
+		/**
+		 * This function is used to save the rt-person post.
+		 *
+		 * @param int     $post_id The post ID.
+		 * @param WP_Post $post    The post object.
+		 * @param bool    $update  Whether this is an existing post being updated or not.
+		 */
+		private function save_rt_person_post( int $post_id, WP_Post $post, bool $update ): void {
+			// Check if our nonce is set.
+			if ( ! isset( $_POST[ 'rt_person_meta_nonce' ] ) ) {
+				return;
+			}
+			//Sanitize nonce
+			$rt_person_meta_nonce = sanitize_text_field( $_POST[ 'rt_person_meta_nonce' ] );
+			// Verify that the nonce is valid.
+			if ( ! wp_verify_nonce( $rt_person_meta_nonce, 'rt_person_meta_nonce' ) ) {
+				return;
+			}
+
+			/** OK, it's safe for us to save the data now. */
+
+			if ( isset( $_POST[ 'rt-person-meta-basic-birth-date' ] ) ) {
+				$rt_person_meta_basic_birth_date = sanitize_text_field( $_POST[ 'rt-person-meta-basic-birth-date' ] );
+				update_post_meta( $post_id, 'rt-person-meta-basic-birth-date', $rt_person_meta_basic_birth_date );
+			}
+
+			if ( isset( $_POST[ 'rt-person-meta-basic-birth-place' ] ) ) {
+				$rt_person_meta_basic_birth_place = sanitize_text_field( $_POST[ 'rt-person-meta-basic-birth-place' ] );
+				if ( ! is_numeric( $rt_person_meta_basic_birth_place ) ) {
+					update_post_meta( $post_id, 'rt-person-meta-basic-birth-place', $rt_person_meta_basic_birth_place );
+				}
+			}
+
+			if ( isset( $_POST[ 'rt-person-meta-social-twitter' ] ) ) {
+				$rt_person_meta_social_twitter = filter_var( $_POST[ 'rt-person-meta-social-twitter' ], FILTER_SANITIZE_URL );
+				if ( filter_var( $rt_person_meta_social_twitter, FILTER_VALIDATE_URL ) ) {
+					update_post_meta( $post_id, 'rt-person-meta-social-twitter', $rt_person_meta_social_twitter );
+				}
+			}
+
+			if ( isset( $_POST[ 'rt-person-meta-social-facebook' ] ) ) {
+				$rt_person_meta_social_facebook = filter_var( $_POST[ 'rt-person-meta-social-facebook' ], FILTER_SANITIZE_URL );
+				if ( filter_var( $rt_person_meta_social_facebook, FILTER_VALIDATE_URL ) ) {
+					update_post_meta( $post_id, 'rt-person-meta-social-facebook', $rt_person_meta_social_facebook );
+				}
+			}
+
+			if ( isset( $_POST[ 'rt-person-meta-social-instagram' ] ) ) {
+				$rt_person_meta_social_instagram = filter_var( $_POST[ 'rt-person-meta-social-instagram' ], FILTER_SANITIZE_URL );
+				if ( filter_var( $rt_person_meta_social_instagram, FILTER_VALIDATE_URL ) ) {
+					update_post_meta( $post_id, 'rt-person-meta-social-instagram', $rt_person_meta_social_instagram );
+				}
+			}
+
+			if ( isset( $_POST[ 'rt-person-meta-social-web' ] ) ) {
+				$rt_person_meta_social_web = filter_var( $_POST[ 'rt-person-meta-social-web' ], FILTER_SANITIZE_URL );
+				if ( filter_var( $rt_person_meta_social_web, FILTER_VALIDATE_URL ) ) {
+					update_post_meta( $post_id, 'rt-person-meta-social-web', $rt_person_meta_social_web );
+				}
+			}
+		}
+
+		/**
+		 * This function is used to save the rt-movie post.
+		 *
+		 * @param int     $post_id The post ID.
+		 * @param WP_Post $post    The post object.
+		 * @param bool    $update  Whether this is an existing post being updated or not.
+		 */
+		private function save_rt_movie_post( int $post_id, WP_Post $post, bool $update ): void {
 
 			// Check if our nonce is set.
 			if ( ! isset( $_POST[ 'rt_movie_meta_nonce' ] ) ) {
@@ -62,31 +140,31 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Save_Post' ) ) {
 
 			$does_any_crew_exist = false;
 			foreach ( $rt_career_terms as $rt_career_term ) {
-				$key = 'rt-movie-meta-crew-' . $rt_career_term->slug;
+				$meta_key = 'rt-movie-meta-crew-' . $rt_career_term->slug;
 				// Make sure that it is set.
-				if ( isset( $_POST[ $key ] ) ) {
-					$does_any_crew_exist = true;
-					$rt_movie_meta_crew  = $_POST[ $key ];
-					if ( is_array( $rt_movie_meta_crew ) && count( $rt_movie_meta_crew ) > 0 ) {
-						$terms = array();
-						foreach ( $rt_movie_meta_crew as $crew ) {
-							// Sanitize user input.
-							$crew = sanitize_text_field( $crew );
-							//If value is emptier than delete the term
-							if ( ! empty( $crew ) ) {
-								$terms[] = $crew;
-							}
-						}
-						$this->set_object_terms( $post_id, $terms, $key );
-					} else {
-						if ( ! empty( $rt_movie_meta_crew ) ) {
+				if ( isset( $_POST[ $meta_key ] ) ) {
+					$does_any_crew_exist     = true;
+					$rt_movie_meta_crew_data = $_POST[ $meta_key ];
+					if ( is_array( $rt_movie_meta_crew_data ) && count( $rt_movie_meta_crew_data ) > 0 ) {
+						$shadow_terms = array();
+						foreach ( $rt_movie_meta_crew_data as $rt_movie_meta_crew ) {
 							// Sanitize user input.
 							$rt_movie_meta_crew = sanitize_text_field( $rt_movie_meta_crew );
-							$this->set_object_terms( $post_id, [ $rt_movie_meta_crew ], $key );
+							//If value is emptier than delete the term
+							if ( ! empty( $rt_movie_meta_crew ) && is_numeric( $rt_movie_meta_crew ) ) {
+								$shadow_terms[] = $rt_movie_meta_crew;
+							}
+						}
+						$this->set_object_terms( $post_id, $shadow_terms, $meta_key );
+					} else {
+						if ( ! empty( $rt_movie_meta_crew_data ) && is_numeric( $rt_movie_meta_crew_data ) ) {
+							// Sanitize user input.
+							$rt_movie_meta_crew_data = sanitize_text_field( $rt_movie_meta_crew_data );
+							$this->set_object_terms( $post_id, [ $rt_movie_meta_crew_data ], $meta_key );
 						}
 					}
 				} else {
-					update_post_meta( $post_id, $key, [] );
+					update_post_meta( $post_id, $meta_key, [] );
 				}
 			}
 
@@ -115,7 +193,7 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Save_Post' ) ) {
 				if ( ! is_numeric( $rt_movie_meta_basic_runtime ) ) {
 					$rt_movie_meta_basic_runtime = (int)$rt_movie_meta_basic_runtime;
 				}
-				if ( $rt_movie_meta_basic_runtime >= 1 && $rt_movie_meta_basic_runtime <= 1000 ){
+				if ( $rt_movie_meta_basic_runtime >= 1 && $rt_movie_meta_basic_runtime <= 1000 ) {
 					update_post_meta( $post_id, 'rt-movie-meta-basic-runtime', $rt_movie_meta_basic_runtime );
 				}
 			}
@@ -128,7 +206,17 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Save_Post' ) ) {
 			}
 		}
 
-		private function set_object_terms( $post_id, $terms, $key ): void {
+		/**
+		 * @function set_object_terms
+		 *           This function is used to update the post meta and set the object terms
+		 *
+		 * @param int    $post_id
+		 * @param mixed  $terms
+		 * @param string $key
+		 *
+		 * @return void
+		 */
+		private function set_object_terms( int $post_id, mixed $terms, string $key ): void {
 			update_post_meta( $post_id, $key, $terms );
 			wp_set_object_terms( $post_id, $terms, '_rt-movie-person', true );
 		}
