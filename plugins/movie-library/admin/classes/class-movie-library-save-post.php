333f805c93d6c925bae246f0889fd595863dccd5
Shalin Shah <sahilshh723@gmail.com>
2023-02-17 13:19:40 +0530

    Function created to set_object_terms

--- plugins/movie-library/admin/classes/class-movie-library-save-post.php
+++ plugins/movie-library/admin/classes/class-movie-library-save-post.php
@@ -20,16 +20,16 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Save_Post' ) ) {
 	 */
 	class Movie_Library_Save_Post {
 		public function save_post( $post_id, $post, $update ): void {
-			if(wp_is_post_autosave($post_id) || wp_is_post_revision($post_id)) {
+			if ( wp_is_post_autosave( $post_id ) || wp_is_post_revision( $post_id ) ) {
 				return;
 			}
 			$post_type = get_post_type( $post_id );
 			// Check the user's permissions.
-			if ( isset( $_POST['post_type'] ) && ($_POST['post_type'] === 'rt-movie' || $post_type === 'rt-movie') ) {
+			if ( isset( $_POST[ 'post_type' ] ) && ( $_POST[ 'post_type' ] === 'rt-movie' || $post_type === 'rt-movie' ) ) {
 				if ( ! current_user_can( 'edit_page', $post_id ) ) {
 					return;
-				}else{
-					$this->save_rt_movie_post($post_id, $post, $update);
+				} else {
+					$this->save_rt_movie_post( $post_id, $post, $update );
 				}
 			} else {
 				if ( ! current_user_can( 'edit_post', $post_id ) ) {
@@ -38,14 +38,14 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Save_Post' ) ) {
 			}
 		}
 
-		private function save_rt_movie_post($post_id, $post, $update): void {
+		private function save_rt_movie_post( $post_id, $post, $update ): void {
 
 			// Check if our nonce is set.
-			if ( ! isset( $_POST['rt_movie_meta_nonce'] ) ) {
+			if ( ! isset( $_POST[ 'rt_movie_meta_nonce' ] ) ) {
 				return;
 			}
 			//Sanitize nonce
-			$rt_movie_meta_nonce = sanitize_text_field( $_POST['rt_movie_meta_nonce'] );
+			$rt_movie_meta_nonce = sanitize_text_field( $_POST[ 'rt_movie_meta_nonce' ] );
 			// Verify that the nonce is valid.
 			if ( ! wp_verify_nonce( $rt_movie_meta_nonce, 'rt_movie_meta_nonce' ) ) {
 				return;
@@ -66,64 +66,71 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Save_Post' ) ) {
 				// Make sure that it is set.
 				if ( isset( $_POST[ $key ] ) ) {
 					$does_any_crew_exist = true;
-					$rt_movie_meta_crew = $_POST[$key];
-					if(is_array($rt_movie_meta_crew) && count($rt_movie_meta_crew) > 0){
+					$rt_movie_meta_crew  = $_POST[ $key ];
+					if ( is_array( $rt_movie_meta_crew ) && count( $rt_movie_meta_crew ) > 0 ) {
 						$terms = array();
-						foreach ($rt_movie_meta_crew as $crew){
+						foreach ( $rt_movie_meta_crew as $crew ) {
 							// Sanitize user input.
-							$crew = sanitize_text_field($crew);
-							//If value is empty than delete the term
-							if(!empty($crew)){
+							$crew = sanitize_text_field( $crew );
+							//If value is emptier than delete the term
+							if ( ! empty( $crew ) ) {
 								$terms[] = $crew;
 							}
-
 						}
-						update_post_meta( $post_id, $key, $terms );
-						wp_set_object_terms($post_id, $terms,'_rt-movie-person', true);
+						$this->set_object_terms( $post_id, $terms, $key );
+					} else {
+						if ( ! empty( $rt_movie_meta_crew ) ) {
+							// Sanitize user input.
+							$rt_movie_meta_crew = sanitize_text_field( $rt_movie_meta_crew );
+							$this->set_object_terms( $post_id, [ $rt_movie_meta_crew ], $key );
+						}
 					}
-				}else{
+				} else {
 					update_post_meta( $post_id, $key, [] );
 				}
 			}
 
-			if (!$does_any_crew_exist){
-				wp_delete_object_term_relationships($post_id, '_rt-movie-person');
+			if ( ! $does_any_crew_exist ) {
+				wp_delete_object_term_relationships( $post_id, '_rt-movie-person' );
 			}
 
 			// Make sure that it is set.
-			if ( isset( $_POST['rt-movie-meta-basic-rating'] ) ) {
+			if ( isset( $_POST[ 'rt-movie-meta-basic-rating' ] ) ) {
 				// Sanitize user input.
-				$rt_movie_meta_basic_rating = sanitize_text_field( $_POST['rt-movie-meta-basic-rating'] );
+				$rt_movie_meta_basic_rating = sanitize_text_field( $_POST[ 'rt-movie-meta-basic-rating' ] );
 				//Validate user input.
 				if ( ! is_numeric( $rt_movie_meta_basic_rating ) ) {
 					$rt_movie_meta_basic_rating = (int)$rt_movie_meta_basic_rating;
 				}
-				if($rt_movie_meta_basic_rating > 5){
-					$rt_movie_meta_basic_rating = 5;
-				}elseif ($rt_movie_meta_basic_rating < 0){
-					$rt_movie_meta_basic_rating = 0;
+				if ( $rt_movie_meta_basic_rating >= 1 && $rt_movie_meta_basic_rating <= 5 ) {
+					// Update the meta field in the database.
+					update_post_meta( $post_id, 'rt-movie-meta-basic-rating', $rt_movie_meta_basic_rating );
 				}
-				update_post_meta( $post_id, 'rt-movie-meta-basic-rating', $rt_movie_meta_basic_rating );
-				// Update the meta field in the database.
 			}
 
-			if ( isset( $_POST['rt-movie-meta-basic-runtime'] ) ) {
+			if ( isset( $_POST[ 'rt-movie-meta-basic-runtime' ] ) ) {
 				// Sanitize user input.
-				$rt_movie_meta_basic_runtime = sanitize_text_field( $_POST['rt-movie-meta-basic-runtime'] );
+				$rt_movie_meta_basic_runtime = sanitize_text_field( $_POST[ 'rt-movie-meta-basic-runtime' ] );
 				//Validate user input.
 				if ( ! is_numeric( $rt_movie_meta_basic_runtime ) ) {
 					$rt_movie_meta_basic_runtime = (int)$rt_movie_meta_basic_runtime;
 				}
-				update_post_meta( $post_id, 'rt-movie-meta-basic-runtime', $rt_movie_meta_basic_runtime );
+				if ( $rt_movie_meta_basic_runtime >= 1 && $rt_movie_meta_basic_runtime <= 1000 ){
+					update_post_meta( $post_id, 'rt-movie-meta-basic-runtime', $rt_movie_meta_basic_runtime );
+				}
 			}
 
-			if ( isset( $_POST['rt-movie-meta-basic-release-date'] ) ) {
+			if ( isset( $_POST[ 'rt-movie-meta-basic-release-date' ] ) ) {
 				// Sanitize user input.
-				$rt_movie_meta_basic_release_date = sanitize_text_field( $_POST['rt-movie-meta-basic-release-date'] );
+				$rt_movie_meta_basic_release_date = sanitize_text_field( $_POST[ 'rt-movie-meta-basic-release-date' ] );
 				// Update the meta field in the database.
 				update_post_meta( $post_id, 'rt-movie-meta-basic-release-date', $rt_movie_meta_basic_release_date );
 			}
+		}
 
+		private function set_object_terms( $post_id, $terms, $key ): void {
+			update_post_meta( $post_id, $key, $terms );
+			wp_set_object_terms( $post_id, $terms, '_rt-movie-person', true );
 		}
 	}
 }
