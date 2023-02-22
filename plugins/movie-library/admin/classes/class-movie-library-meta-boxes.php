e98f5543eef1fbb92d488693e914c2f6ec235abb
Shalin Shah <sahilshh723@gmail.com>
2023-02-22 18:11:05 +0530

    Add Videos text is added into the meta boxes

--- plugins/movie-library/admin/classes/class-movie-library-meta-boxes.php
+++ plugins/movie-library/admin/classes/class-movie-library-meta-boxes.php
@@ -291,7 +291,7 @@ if ( ! class_exists( 'MovieLib\admin\classes\Movie_Library_Meta_Boxes' ) ) {
 
 				<button class = "rt-media-meta-add rt-media-meta-add rt-media-meta-videos-add"
 							type = "button">
-						Add
+						<?php esc_html_e( 'Add Videos', 'movie-library' ); ?>
 				</button>
 
 				<input name = "rt-media-meta-selected-videos" hidden = "hidden">
