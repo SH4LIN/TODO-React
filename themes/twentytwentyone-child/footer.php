06094637ea6804c2d4f2bbe820a446eb4103d372
Shalin Shah <sahilshh723@gmail.com>
2023-03-13 02:34:28 +0530

    Feature: Plugin Changes:
    - archive-movie.css: Padding added for the genre for large screens.
    
    - footer.css: changed to have the footer responsive.
    
    - footer.php: Reworked tbe footer.
    
    - header.css: Header URL decoration is set to none
    
    - New svg added of social media.

--- themes/twentytwentyone-child/footer.php
+++ themes/twentytwentyone-child/footer.php
@@ -7,117 +7,105 @@
  * @since Twenty Twenty-One Child 1.0
  */
 
-$cst_footer_logo = has_custom_logo() ? get_custom_logo() : '<h2 class="st-header-logo">Screen <span>Time</span></h2>';
-$privacy_link    = get_privacy_policy_url( '<div class="privacy-policy">', '</div>' );
-
 ?>
 
-				</main><!-- #main -->
-			</div><!-- #primary -->
-		</div><!-- #content -->
-			<footer class="st-footer"> <!-- .st-footer -->
-				<div class="st-container">
-					<div class="st-footer-inner">
-						<div class="st-footer-info">
-							<div class="st-footer-logo">
-								<?php echo $cst_footer_logo; ?>
-							</div>
-							<div class="st-footer-social">
-								<p class="st-footer-social-heading">
-									<?php esc_html_e( 'Follow us', 'screen-time' ); ?>
-								</p>
-								<ul class="st-footer-social-list">
-									<li class="st-footer-social-list-item">
-										<a href="https://www.instagram.com/screentimeapp/" target="_blank" rel="noopener noreferrer">
-											<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/instagram.svg'; ?>" alt="Instagram">
-										</a>
-									</li>
-									<li class="st-footer-social-list-item">
-										<a href="https://www.instagram.com/screentimeapp/" target="_blank" rel="noopener noreferrer">
-											<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/instagram.svg'; ?>" alt="Instagram">
-										</a>
-									</li>
-									<li class="st-footer-social-list-item">
-										<a href="https://www.instagram.com/screentimeapp/" target="_blank" rel="noopener noreferrer">
-											<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/instagram.svg'; ?>" alt="Instagram">
-										</a>
-									</li>
-									<li class="st-footer-social-list-item">
-										<a href="https://www.youtube.com/channel/UCZ1Z1YQ1Q9Z1Z1YQ1Q9Z1Z1" target="_blank" rel="noopener noreferrer">
-											<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/youtube.svg'; ?>" alt="Youtube">
-										</a>
-									</li>
-									<li class="st-footer-social-list-item">
-										<a href="#" target="_blank" rel="noopener noreferrer">
-											<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/rss.svg'; ?>" alt="Linkedin">
-										</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<div class="st-footer-col">
-							<?php if ( has_nav_menu( 'footer-col-1' ) ) : ?>
-								<nav aria-label="<?php esc_attr_e( 'Footer menu', 'screen-time' ); ?>" class="st-footer-navigation">
-									<p class="st-footer-nav-title">
-										<?php echo wp_get_nav_menu_name( 'footer-col-1' ); ?>
-									</p>
-									<ul class="st-footer-nav-wrapper">
-										<?php
-											wp_nav_menu(
-												array(
-													'theme_location' => 'footer-col-1',
-													'items_wrap'     => '%3$s',
-													'container'      => false,
-													'depth'          => 1,
-													'link_before'    => '<span>',
-													'link_after'     => '</span>',
-													'fallback_cb'    => false,
-												)
-											);
-										?>
-									</ul><!-- .footer-navigation-wrapper -->
-								</nav><!-- .footer-navigation -->
-							<?php endif; ?>
-						</div>
-						<div class="st-footer-col">
-							<?php if ( has_nav_menu( 'footer-col-2' ) ) : ?>
-								<nav aria-label="<?php esc_attr_e( 'Footer menu', 'screen-time' ); ?>" class="st-footer-navigation">
-									<p class="st-footer-nav-title">
-										<?php echo wp_get_nav_menu_name( 'footer-col-1' ); ?>
-									</p>
-									<ul class="st-footer-nav-wrapper">
-										<?php
-											wp_nav_menu(
-												array(
-													'theme_location' => 'footer-col-2',
-													'items_wrap'     => '%3$s',
-													'container'      => false,
-													'depth'          => 1,
-													'link_before'    => '<span>',
-													'link_after'     => '</span>',
-													'fallback_cb'    => false,
-												)
-											);
-										?>
-									</ul><!-- .footer-navigation-wrapper -->
-								</nav><!-- .footer-navigation -->
-							<?php endif; ?>
-						</div>
-					</div>
-					<div class="st-footer-powered-by"><!-- .powered-by -->
-					   <p>
-						<?php
-							printf(
-								/* translators: %s: Privacy Policy. */
-								esc_html( '© 2022 Lifestyle Magazine. All Rights Reserved. Terms of Service  |  %s' ),
-								'<a href="' . esc_url( $privacy_link ) . '">Privacy Policy</a>'
-							);
-							?>
-					   </p>
-					</div><!-- .powered-by -->
-				</div>
-			</footer><!-- .st-footer -->
-	</div><!-- #page -->
+<footer>
+	<div class="st-footer-container">
+		<div class="st-footer-content-container">
+			<div class="st-footer-logo-text-container">
+				<span class="primary-text-secondary-font st-footer-logo-first-half-text">
+					<?php esc_html_e( 'Screen', 'screen-time' ); ?>
+				</span>
+
+				<span class="primary-text-secondary-font st-footer-logo-second-half-text">
+					<?php esc_html_e( 'Time', 'screen-time' ); ?>
+				</span>
+			</div>
+
+			<div class="primary-text-primary-font st-footer-follow-text">
+				<?php esc_html_e( 'Follow us', 'screen-time' ); ?>
+			</div>
+
+			<div class="st-footer-social-container">
+				<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/ic_facebook_filled.svg' ); ?>" />
+				<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/ic_twitter_filled.svg' ); ?>" />
+				<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/ic_youtube_filled.svg' ); ?>" />
+				<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/ic_instagram_filled.svg' ); ?>" />
+				<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/ic_rss_filled.svg' ); ?>" />
+			</div>
+		</div>
+
+			<?php
+			$company_menu = wp_nav_menu(
+				array(
+					'menu'            => 'company',
+					'theme_location'  => 'footer',
+					'menu_class'      => 'st-company-navigation-list',
+					'container'       => 'nav',
+					'container_class' => 'st-company-navigation',
+					'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+					'fallback'        => false,
+					'echo'            => false,
+				)
+			);
+
+			if ( $company_menu ) :
+				?>
+			<div class="st-footer-company-navigation-container">
+				<span class="primary-text-primary-font st-footer-company-navigation">
+					<?php esc_html_e( 'Company', 'screen-time' ); ?>
+				</span>
+
+				<?php echo $company_menu; ?>
+			</div>
+
+			<?php endif; ?>
+
+
+		<div class="st-footer-explore-navigation-container">
+			<?php
+			$explore_menu = wp_nav_menu(
+				array(
+					'menu'            => 'explore',
+					'theme_location'  => 'footer',
+					'menu_class'      => 'st-explore-navigation-list',
+					'container'       => 'nav',
+					'container_class' => 'st-explore-navigation',
+					'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+					'fallback'        => false,
+					'echo'            => false,
+				)
+			);
+
+			if ( $explore_menu ) :
+				?>
+				<div class="st-footer-company-navigation-container">
+				<span class="primary-text-primary-font st-footer-company-navigation">
+					<?php esc_html_e( 'Explore', 'screen-time' ); ?>
+				</span>
+
+					<?php echo $explore_menu; ?>
+			</div>
+
+			<?php endif; ?>
+		</div>
+
+
+	</div>
+
+	<div class="st-footer-separator"></div>
+
+		<div class="primary-text-primary-font st-footer-rights-reserved-text">
+			<?php
+			esc_html_e(
+				'© 2022 ScreenTime. All Rights Reserved.
+					 Terms of Service  |  Privacy Policy',
+				'screen-time'
+			);
+			?>
+		</div>
+</footer>
+
 
 <?php wp_footer(); ?>
 
