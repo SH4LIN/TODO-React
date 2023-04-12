44c9881bc7415a728581a5d2196e063f27648817
Shalin Shah <sahilshh723@gmail.com>
2023-04-12 22:53:40 +0530

    feature/custom-wp-list-commands:
    - class-mlb-export-command.php: Making CSS using the WP_Filesystem API, Added the progressbar.

--- plugins/movie-library/admin/classes/custom-commands/class-mlb-export-command.php
+++ plugins/movie-library/admin/classes/custom-commands/class-mlb-export-command.php
@@ -13,6 +13,8 @@
 use MovieLib\admin\classes\custom_post_types\RT_Movie;
 use MovieLib\admin\classes\custom_post_types\RT_Person;
 
+use function WP_CLI\Utils\make_progress_bar;
+
 defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'MovieLib\admin\classes\custom_commands\MLB_Export_Command' ) ) :
@@ -40,7 +42,11 @@ if ( ! class_exists( 'MovieLib\admin\classes\custom_commands\MLB_Export_Command'
 		 * @param array $assoc_args Associative arguments.
 		 */
 		public function __invoke( $args, $assoc_args ) {
-			$post_type = $args[0];
+			if ( ! empty( $args ) ) {
+				$post_type = $args[0];
+			} else {
+				$post_type = '';
+			}
 
 			if ( RT_Movie::SLUG === $post_type ) {
 				$movie_export = $this->export_custom_posts( $post_type );
@@ -57,14 +63,15 @@ if ( ! class_exists( 'MovieLib\admin\classes\custom_commands\MLB_Export_Command'
 					WP_CLI::success( 'rt-person post type exported successfully.' );
 				}
 			} else {
-				$movie_export  = $this->export_custom_posts( RT_Movie::SLUG );
-				$person_export = $this->export_custom_posts( RT_Person::SLUG );
+				$movie_export = $this->export_custom_posts( RT_Movie::SLUG );
 
 				if ( ! $movie_export ) {
 					WP_CLI::error( 'Error while exporting rt-movie post type.' );
 				} else {
 					WP_CLI::success( 'rt-movie post type exported successfully.' );
 				}
+
+				$person_export = $this->export_custom_posts( RT_Person::SLUG );
 
 				if ( ! $person_export ) {
 					WP_CLI::error( 'Error while exporting rt-person post type.' );
@@ -75,12 +82,13 @@ if ( ! class_exists( 'MovieLib\admin\classes\custom_commands\MLB_Export_Command'
 		}
 
 		/**
-		 * Exports rt-movie post type in csv file.
+		 * Exports rt-movie and rt-person post type in csv file.
 		 *
 		 * @param string $type Post type to export.
 		 * @return bool
 		 */
 		private function export_custom_posts( $type ) {
+			// Making Optimised WP_Query to fetch the post.
 			$custom_posts = get_posts(
 				array(
 					'post_type'      => $type,
@@ -111,18 +119,63 @@ if ( ! class_exists( 'MovieLib\admin\classes\custom_commands\MLB_Export_Command'
 			// Get the properties of first object.
 			$csv_header = array_keys( get_object_vars( $custom_posts[0] ) );
 
-			// Write to csv file.
-			$fp = fopen( "$type.csv", 'w' );
-			fputcsv( $fp, $csv_header );
-			foreach ( $custom_posts as $custom_post ) {
-				if ( false === fputcsv( $fp, get_object_vars( $custom_post ) ) ) {
-					unlink( "$type.csv" );
-					return false;
-				}
+			$creds = request_filesystem_credentials( admin_url(), '', false, false );
+			if ( false === $creds ) {
+				return false;
 			}
-			fclose( $fp );
+
+			if ( ! WP_Filesystem( $creds ) ) {
+				return false;
+			}
+			global $wp_filesystem;
+
+			$csv    = '';
+			$count  = count( $custom_posts );
+			$count += 2;
+
+			// translators: %s is the post type.
+			$progress = make_progress_bar( sprintf( __( 'Exporting %s', 'movie-library' ), $type ), $count );
+
+			$progress->tick();
+			$csv .= $this->get_data_in_csv( $csv_header );
+			foreach ( $custom_posts as $custom_post ) {
+				$csv .= $this->get_data_in_csv( get_object_vars( $custom_post ) );
+				$progress->tick();
+			}
+
+			$progress->tick();
+			$wp_filesystem->put_contents( "$type.csv", $csv, FS_CHMOD_FILE );
+			$progress->finish();
 
 			return true;
+		}
+
+		/**
+		 * This function is used to convert data into csv format.
+		 *
+		 * @param array $data_array to be converted into CSV format.
+		 *
+		 * @return string
+		 */
+		private function get_data_in_csv( $data_array ) {
+			$fd   = ',';
+			$quot = '"';
+			$str  = '';
+
+			foreach ( $data_array as $data ) {
+				$data = str_replace(
+					array( $quot, "\n" ),
+					array( $quot . $quot, '' ),
+					$data
+				);
+				if ( strchr( $data, $fd ) !== false || strchr( $data, $quot ) !== false ) {
+					$str .= $quot . $data . $quot . $fd;
+				} else {
+					$str .= $data . $fd;
+				}
+			}
+
+			return substr( $str, 0, -1 ) . "\n";
 		}
 	}
 endif;
